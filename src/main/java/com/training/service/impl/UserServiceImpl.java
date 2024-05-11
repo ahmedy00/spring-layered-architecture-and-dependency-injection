679117ccd3f32f2f2c4a91e011226e21752820fa
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-12 00:52:37 +0300

    Validation and error handling.

--- src/main/java/com/training/service/impl/UserServiceImpl.java
+++ src/main/java/com/training/service/impl/UserServiceImpl.java
@@ -1,13 +1,24 @@
 package com.training.service.impl;
 
 import com.training.dto.User;
+import com.training.repository.UserRepository;
 import com.training.service.UserService;
 import org.springframework.stereotype.Service;
 
+import java.security.InvalidParameterException;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
     @Override
     public User getUserById(Long userId) {
-        return null;
+        // Validation
+        if (userId == null) {
+            throw new InvalidParameterException("Invalid user ID");
+        }
+        // TODO: Add custom exception.
+        return userRepository.findById(userId).orElseThrow(null);
     }
 }
