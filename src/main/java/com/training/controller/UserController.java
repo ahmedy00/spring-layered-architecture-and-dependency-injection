790df5079db69d4bd26e5e3dc45481120845815d
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-12 21:53:55 +0300

    Updated User Controller: Added new endpoints.

--- src/main/java/com/training/controller/UserController.java
+++ src/main/java/com/training/controller/UserController.java
@@ -3,10 +3,9 @@ package com.training.controller;
 import com.training.entity.User;
 import com.training.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +17,31 @@ public class UserController {
     @GetMapping("/users/{userId}")
     public User getUserById(@PathVariable("userId") Long userId) {
         return userService.getUserById(userId);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void removeUser(@PathVariable Long userId) {
+        userService.removeUser(userId);
+    }
+
+    @PutMapping("/users/{userId}")
+    public User updateUser(@PathVariable Long userId, User user) {
+        return userService.updateUser(userId, user);
+    }
+
+    @PostMapping("/users/custom")
+    public User createUserWithCustomQuery(@RequestBody User user) {
+        return userService.createUserWithCustomQuery(user.getUserId(), user.getEmail(), user.getFirstName(), user.getLastName(), user.getUsername());
     }
 
 }
