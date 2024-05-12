d57c86203281742c30e12c3ac8d2e9cf87e20ee0
ahmet <dede_dede_ahmet@hotmail.com>
2024-05-12 21:55:21 +0300

    Updated User Repository: Added custom query for insert entity to database.

--- src/main/java/com/training/repository/UserRepository.java
+++ src/main/java/com/training/repository/UserRepository.java
@@ -1,8 +1,18 @@
 package com.training.repository;
 
 import com.training.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO _user (user_id, email, first_name, last_name, username) VALUES (:userId, :email, :firstName, :lastName, :username)", nativeQuery = true)
+    int createUserWithCustomQuery(Long userId, String email, String firstName, String lastName, String username);
 
 }
